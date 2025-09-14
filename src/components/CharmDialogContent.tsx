404895ab9e5382dd4dee8ddf96653664946828de
AlexTheMagnus <malejandrolule@gmail.com>
2025-09-14 20:57:11 +0200

    feat: add scroll on desktop

--- src/components/CharmDialogContent.tsx
+++ src/components/CharmDialogContent.tsx
@@ -56,7 +56,7 @@ export function CharmDialogContent({
 
             <div
                 className={`flex w-full ${
-                    isMobile ? "flex-col gap-10 items-center" : "h-full gap-30"
+                    isMobile ? "flex-col gap-10 items-center" : "h-full gap-10"
                 } `}
             >
                 <DialogColumn>
--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { XIcon } from "lucide-react";
 
-import { useMobile } from "@/lib/MobileContext";
 import { mergeClasses } from "@/lib/utils";
 
 const Dialog = (props: React.ComponentProps<typeof DialogPrimitive.Root>) => (
@@ -45,16 +44,13 @@ const DialogContent = ({
 }: React.ComponentProps<typeof DialogPrimitive.Content> & {
     showCloseButton?: boolean;
 }) => {
-    const isMobile = useMobile();
-
     return (
         <DialogPortal data-slot="dialog-portal">
             <DialogOverlay />
             <DialogPrimitive.Content
                 data-slot="dialog-content"
                 className={mergeClasses(
-                    "flex flex-col gap-20 bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] rounded-lg border p-6 shadow-lg duration-200",
-                    isMobile ? "overflow-scroll" : "",
+                    "flex flex-col gap-20 bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] rounded-lg border p-6 shadow-lg duration-200 overflow-scroll",
                     className
                 )}
                 {...props}
